c22635280aebb4cf72ed916fb8de374e3ec0e492
Ayelet <ayulacan@gmail.com>
2018-06-24 16:05:38 +0300

    test

--- components/About/aboutController.js
+++ components/About/aboutController.js
@@ -1,6 +1,7 @@
 angular.module('citiesApp')
 .controller('aboutController', ['$scope', function($scope) {
     $scope.count = 0;
+    $scope.guest = 'guest';
     $scope.myFunc = function() {
       $scope.count++;
     };

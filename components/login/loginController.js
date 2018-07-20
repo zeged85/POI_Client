47f65584d0bd6ee40e11e4a0d79fa806193fb6ef
Nir Zagdanski <zeged@yahoo.com>
2018-07-21 02:12:49 +0300

    login

--- components/login/loginController.js
+++ components/login/loginController.js
@@ -1,5 +1,31 @@
 angular.module('citiesApp')
-    .controller('loginController',['$scope', function ($scope) {
+    .controller('loginController',['$http','setHeadersToken', function ($http,setHeadersToken) {
         self = this;
-        self.recover;
+        
+        self.username = "";
+        self.password = "";
+        
+        self.submit = function () {
+            let user = {
+                username : self.username,
+                password : self.password
+            }
+            console.log('User clicked submit with ', user);
+            let serverUrl = 'http://localhost:4000/'
+            $http.post(serverUrl + "Users/log", user)
+            .then(function (response) {
+                //First function handles success
+                //self.login.content = response.data.token;
+                //setHeadersToken.set(self.login.content)
+                setHeadersToken.set(response.data)
+                console.log(response.data)
+
+            }, function (response) {
+                //Second function handles error
+               // self.login.content = "Something went wrong";
+               console.log(response)
+            });
+        };
+
+
     }]);

9b846d5bef75214b89a1f83e8188b9e4bbb023d7
Nir Zagdanski <zeged@yahoo.com>
2018-07-21 16:26:42 +0300

    register

--- components/register/registerController.js
+++ components/register/registerController.js
@@ -3,6 +3,12 @@ angular.module('citiesApp')
 
         self = this;
 
+        self.countries = ['a','b']
+        
+        self.category1 = false;
+        self.category2 = false;
+        self.category3 = false;
+        self.category4 = false;
         
 
         let serverUrl = 'http://localhost:4000/'
@@ -15,6 +21,9 @@ angular.module('citiesApp')
            // $scope.indxCtrl.username = self.username
            self.Q1 = response.data.questions[0]
            self.Q2 = response.data.questions[1]
+           
+           self.countries =response.data.countries
+           
             console.log(response.data)
 
         }, function (response) {
@@ -24,6 +33,8 @@ angular.module('citiesApp')
         });
 
         self.submit = function () {
+            console.log("selected:" + self.selectedCountry)
+            self.country = self.selectedCountry
             let user = {
                 username : self.username,
                 firstName : self.firstName,
@@ -31,18 +42,22 @@ angular.module('citiesApp')
                 password : self.password,
                 email : self.email,
                 country : self.country,
-
-
+                category1 : self.category1,
+                category2 : self.category2,
+                category3 : self.category3,
+                category4 : self.category4,
+                answer1 : self.answer1,
+                answer2 : self.answer2
+                
             }
+
             console.log('User clicked submit with ', user);
             let serverUrl = 'http://localhost:4000/'
-            $http.post(serverUrl + "Users/log", user)
+            $http.post(serverUrl + "Users/reg", user)
             .then(function (response) {
                 //First function handles success
                 //self.login.content = response.data.token;
                 //setHeadersToken.set(self.login.content)
-                setHeadersToken.set(response.data)
-                $scope.indxCtrl.username = self.username
                 console.log(response.data)
 
             }, function (response) {

e920e8fd3f61c9f6950c786c77e10830ece4ab5a
Ayelet <ayulacan@gmail.com>
2018-07-23 21:01:24 +0300

    home page

--- components/home/homeController.js
+++ components/home/homeController.js
@@ -1,5 +1,44 @@
 angular.module('citiesApp')
-    .controller('homeController', [ function () {
+    .controller('homeController', ['poiService', 'favService', function (poiService, favService) {
         self = this;
+
+        self.sites = []
+        self.pois = [] //favs
+
+        poiService.get()
+        favService.get()
+        self.poiArray = []
+        length = self.pois.length
+
+        if(self.pois.length <= 0)
+            self.favExsist = false
+            else
+            self.favExsist = true
+
+        id1 = self.pois[length-1]
+        id2 = self.pois[length-2]
+        setTimeout(function(){
+            for (i = 0; i < self.sites.length; i++) {
+                if (self.sites[i].id === id1) {
+                    index1 = i
+                }
+                if (self.sites[i].id === id2) {
+                    index2 = i
+                }
+            }
+    
+
+
+        }, 1000)
+        self.poi1 = self.sites[index1]
+        self.poi2 = self.sites[index2]
+        self.poiArray.push(self.poi1)
+        self.poiArray.push(self.poi2)
+       
+        self.selectedSite = function (site) {
+            poiService.setPoi(site)
+           
+        }
+
 
     }]);
--- components/login/loginController.js
+++ components/login/loginController.js
@@ -1,44 +1,50 @@
 angular.module('citiesApp')
-    .controller('loginController',['$http','setHeadersToken','$scope','$location','$rootScope','localStorageModel', function ($http,setHeadersToken,$scope,$location,$rootScope,localStorageModel) {
+    .controller('loginController', ['$http', 'setHeadersToken', '$scope', '$location', '$rootScope', 'localStorageModel', function ($http, setHeadersToken, $scope, $location, $rootScope, localStorageModel) {
         self = this;
-        
+
         self.username = "dfgdfg";
         self.password = "";
-        
+
 
         self.submit = function () {
             let user = {
-                username : self.username,
-                password : self.password
+                username: self.username,
+                password: self.password
             }
             console.log('User clicked submit with ', user);
             let serverUrl = 'http://localhost:4000/'
             $http.post(serverUrl + "Users/log", user)
-            .then(function (response) {
-                //First function handles success
-                //self.login.content = response.data.token;
-                //setHeadersToken.set(self.login.content)
-                setHeadersToken.set(response.data)
-                $scope.indxCtrl.userName = self.username
-                $scope.indxCtrl.guest = false
+                .then(function (response) {
+                    //First function handles success
+                    //self.login.content = response.data.token;
+                    //setHeadersToken.set(self.login.content)
+                    setHeadersToken.set(response.data)
+                    $scope.indxCtrl.userName = self.username
+                    $scope.indxCtrl.guest = false
 
-                $rootScope.login = true
+                    $rootScope.login = true
 
-                localStorageModel.removeLocalStorage("username")
-                localStorageModel.addLocalStorage("username",self.username)
+                    localStorageModel.removeLocalStorage("username")
+                    localStorageModel.addLocalStorage("username", self.username)
 
-                localStorageModel.removeLocalStorage("token")
-                localStorageModel.addLocalStorage("token",response.data)
-                
-                console.log(response.data)
-               $location.path('/home')
+                    localStorageModel.removeLocalStorage("token")
+                    localStorageModel.addLocalStorage("token", response.data)
 
-            }, function (response) {
-                //Second function handles error
-               // self.login.content = "Something went wrong";
-               console.log(response)
-            });
+                    console.log(response.data)
+                    $location.path('/home')
+
+                }, function (response) {
+                    //Second function handles error
+                    // self.login.content = "Something went wrong";
+                    console.log(response)
+                });
         };
-
+        self.recover = false
+        self.forgot = function () {
+            if (self.recover === true)
+                self.recover = false
+            else if (self.recover === false)
+                self.recover = true
+        }
 
     }]);
